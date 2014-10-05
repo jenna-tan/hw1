1ff907df7b62d75734c776ea2c523216b01da373
Jenna Tan <jenna.r.tan@gmail.com>
2014-10-04 22:54:44 -0400

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,14 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-  end
+  end 
 
+  def new
+    @user = User.new
+  end
+  
   def show
-    User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def create
@@ -18,8 +22,9 @@ class UsersController < ApplicationController
     end
   end
 
+
   private
     def user_params
-      params.require(:user).permit(:name)
+      params.require(:user).permit(:name, :age, :major)
     end
 end
